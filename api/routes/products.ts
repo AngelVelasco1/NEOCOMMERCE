ba60b76fb92fcd85ecf9700612802ab3f11b7b69
Angel Velasco <angelgg2020@outlook.com>
2025-03-28 18:16:38 -0500

    feat: :building_construction: Divide packages to frontend and backend

--- api/routes/products.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProducts, getLatestProducts } from '../controllers/products';
-
-export const productsRoutes = () => {
-    const app = Router();
-    app.get("/products", getProducts)
-    app.get("/latestProducts", getLatestProducts)
-    return app;
-}
--- api/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { productsRoutes } from './products.js';
-
-export const initRoutes = () => {
-    const app = Router();
-    app.use("/use", productsRoutes())
-    return app;
-}
