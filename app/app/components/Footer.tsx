a1823deea46fa4b8e3a75ad63a8f01ebcf3abaad
Jose Álvarez <josealvarezd9@outlook>
2025-05-21 16:05:39 -0500

    CREATE LOGIN AND REGISTER

--- app/app/components/Footer.tsx
+++ app/app/components/Footer.tsx
@@ -4,11 +4,12 @@ import { FaPhoneAlt } from "react-icons/fa";
 import React from 'react';
 
 export const Footer = () => {
+  const datefullyear = new Date().getFullYear();
   return (
       <footer className="container z-30 flex flex-col bg-transparent items-center justify-center md:flex-row mx-auto p-10 text-slate-700 ">
         <div className="gap-y-2 flex flex-col">
-          <h3 className="text-2xl text-center font-bold uppercase">Sensual Candles</h3>
-          <p className="text-center text-sm">Copyright © 2024 SENSUAL CANDLES . Todos los derechos Reservados.</p>
+          <h3 className="text-2xl text-center font-bold uppercase">NEOCOMMERCE</h3>
+          <p className="text-center text-sm">Copyright © {datefullyear} . Todos los derechos Reservados.</p>
           <div className="flex items-center justify-center space-x-4 mt-2">
             <button className="">
               <FaFacebookF className="inline transition hover:opacity-75" size="1.45em" />
